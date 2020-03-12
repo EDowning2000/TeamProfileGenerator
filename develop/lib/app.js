2cfff59a87f107f2061b30340560c3554f46e79b
EDowning2000 <emersondowning@gmail.com>
2020-03-11 20:36:33 -0400

    initial prompts are added

--- develop/lib/app.js
+++ develop/lib/app.js
@@ -9,6 +9,37 @@ const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 ​
 const render = require("./htmlRenderer");
+
+const promptManager = [{
+  type: "input",
+  message : "What is your managers name?", 
+  name : "managerName"
+},{
+  type: 'input',
+  message : "What is their ID?",
+  name : "managerID"
+},{
+  type : 'input',
+  message :"What is their email?",
+  name : "managerEmail"
+},{
+  type : 'input',
+  message: "What is their office number?",
+  name : "managerOffice"
+},{
+    type: 'list',
+    message: 'What type of employee would you like to add next?',
+    name: 'role',
+    choices: [
+        "manager",
+        "engineer",
+        "intern",
+        "No more employees"
+    ]
+}
+
+
+]
 ​
 ​
 // Write code to use inquirer to gather information about the development team members,
