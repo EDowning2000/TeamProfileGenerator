7bba7153a3c1256e601def2a629551063307531d
EDowning2000 <emersondowning@gmail.com>
2020-03-13 12:18:41 -0400

    questions are being asked in the correct order, and switching with the switch class

--- develop/lib/app.js
+++ develop/lib/app.js
@@ -4,74 +4,118 @@ const Intern = require("./Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-​
 const OUTPUT_DIR = path.resolve(__dirname, "output")
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-​
 const render = require("./htmlRenderer");
+const manager = []
+const intern = []
+const engineer = []
 
-const promptManager = [{
+function initialManagerPrompt () { 
+  inquirer.prompt([{
   type: "input",
   message : "What is your managers name?", 
   name : "managerName"
 },{
-  type: 'input',
+  type: "input",
   message : "What is their ID?",
   name : "managerID"
 },{
-  type : 'input',
+  type : "input",
   message :"What is their email?",
   name : "managerEmail"
 },{
-  type : 'input',
+  type : "input",
   message: "What is their office number?",
   name : "managerOffice"
-},{
-    type: 'list',
-    message: 'What type of employee would you like to add next?',
-    name: 'role',
-    choices: [
-        "manager",
-        "engineer",
-        "intern",
-        "No more employees"
-    ]
+}])
+.then(data=>{
+  manager.push(data)
+  console.log(manager)
+  nextEmployee()
+  
+})
 }
 
-function nextEmployee(){
-  inquirer.prompt([{
-    ype: 'list',
-    message: 'What type of employee would you like to add next?',
-    name: 'role',
+function nextEmployee() { 
+  inquirer.prompt({
+    type: "list",
+    message: "What type of employee would you like to add next?",
+    name: "role",
     choices: [
         "manager",
         "engineer",
         "intern",
         "No more employees"
-    ]
-  }])  
-}
-​
-​
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-​
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-​
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-​
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-​
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an 
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work!```
+    ]})
+    .then(data=>{
+      const role = data.role
+      switch(role){
+        case "Manager": 
+          initialManagerPrompt();
+          break;
+        case "Intern":
+          internPrompt();
+          break;
+        case "Engineer":
+          engineerPrompt();
+          break;
+        case "No more employees":
+          return "Roster Completed";
+    }
+    })
+  }
+
+  function internPrompt() { 
+    inquirer.prompt([{
+      type: "input",
+  message : "What is your interns name?", 
+  name : "internName"
+},{
+  type: "input",
+  message : "What is their ID?",
+  name : "internID"
+},{
+  type : "input",
+  message :"What is their email?",
+  name : "internEmail"
+},{
+  type : "input",
+  message: "What school are they from?",
+  name : "internSchool"
+}])
+.then(data=>{
+  intern.push(data)
+  console.log(intern)
+  nextEmployee()
+  })
+
+
+function engineerPrompt() { 
+  inquirer.prompt([{
+    type: "input",
+message : "What is your engineeers name?", 
+name : "enginnerName"
+},{
+type: "input",
+message : "What is their ID?",
+name : "enginnerID"
+},{
+type : "input",
+message :"What is their email?",
+name : "enginnerEmail"
+},{
+type : "input",
+message: "What is their github username?",
+name : "enginnerGithub"
+}])
+.then(data=>{
+  engineer.push(data)
+  console.log(engineer)
+  nextEmployee()
+})
+}}
+
+
+
+initialPrompt()
