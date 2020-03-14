727f79252c0ada4ba6ff765cbdc1fae23a13973e
Emerson Downing <57017788+EDowning2000@users.noreply.github.com>
2020-03-13 21:55:32 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+const Manager = require("./Manager");
+const Engineer = require("./Engineer");
+const Intern = require("./Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const OUTPUT_DIR = path.resolve(__dirname, "output")
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+const render = require("./htmlRenderer");
+// const manager = []
+// const intern = []
+// const engineer = []
+const team = []
+
+function initialManagerPrompt () { 
+  inquirer.prompt([{
+  type: "input",
+  message : "What is your managers name?", 
+  name : "managerName"
+},{
+  type: "input",
+  message : "What is their ID?",
+  name : "managerID"
+},{
+  type : "input",
+  message :"What is their email?",
+  name : "managerEmail"
+},{
+  type : "input",
+  message: "What is their office number?",
+  name : "managerOffice"
+}])
+.then(data=>{
+  // manager.push(data)
+  // console.log(manager)
+  const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOffice)
+  team.push(manager)
+  nextEmployee()
+})
+}
+
+function nextEmployee() { 
+  inquirer.prompt({
+    type: "list",
+    message: "What type of employee would you like to add next?",
+    name: "role",
+    choices: [
+        "Manager",
+        "Engineer",
+        "Intern",
+        "No more employees"
+    ]})
+    .then(data=>{
+
+      const role = data.role
+      switch(role){
+        case "Manager": 
+          initialManagerPrompt();
+          break;
+        case "Intern":
+          internPrompt();
+          break;
+        case "Engineer":
+          engineerPrompt();
+          break;
+        case "No more employees":
+          renderer();
+        break
+    }
+    })
+  }
+
+  function internPrompt() { 
+    inquirer.prompt([{
+      type: "input",
+  message : "What is your interns name?", 
+  name : "internName"
+},{
+  type: "input",
+  message : "What is their ID?",
+  name : "internID"
+},{
+  type : "input",
+  message :"What is their email?",
+  name : "internEmail"
+},{
+  type : "input",
+  message: "What school are they from?",
+  name : "internSchool"
+}])
+.then(data=>{
+  const intern = new Intern(data.internName, data.internID, data.internEmail, data.internSchool)
+  team.push(intern)
+  nextEmployee()
+  })
+  }
+
+function engineerPrompt() { 
+  inquirer.prompt([{
+    type: "input",
+message : "What is your engineeers name?", 
+name : "enginnerName"
+},{
+type: "input",
+message : "What is their ID?",
+name : "enginnerID"
+},{
+type : "input",
+message :"What is their email?",
+name : "enginnerEmail"
+},{
+type : "input",
+message: "What is their github username?",
+name : "enginnerGithub"
+}])
+.then(data=>{
+  const manager = new Manager(data.managerName, data.managerID, data.managerEmail, data.managerOffice)
+  team.push(manager)
+  nextEmployee()
+})
+}
+  
+
+
+
+function renderer(){
+  fs.writeFile(outputPath, render(team), (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+})
+}
+initialManagerPrompt();
